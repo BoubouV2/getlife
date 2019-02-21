212d40808efb08a8257707e2c026470e0bb5e8be
BoubouV2 <47854711+BoubouV2@users.noreply.github.com>
2019-02-22 00:16:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,7 +14,7 @@ client.on('ready', function () {
 
     var games = [
         "!help | GetLife",
-        "Merci Zid !",
+        "Je t'aime Zid <3",
     ]
     client.user.setActivity(setInterval(function() {
     client.user.setActivity(games[Math.floor(Math.random() * games.length)], {url:"https://www.twitch.tv/deax_licorne", type: "STREAMING"})}, 10000))
