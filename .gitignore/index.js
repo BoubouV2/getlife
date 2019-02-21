e90db86701af8c748f5603fde79c083009842a22
BoubouV2 <47854711+BoubouV2@users.noreply.github.com>
2019-02-21 13:28:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,26 @@
 const Discord = require("discord.js");
 
+var PREFIX = "!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("GetLife, !help");
     console.log("Je suis bien présent :)");
 });
+
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.cleanContent.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("GetLife teste message");
+        break;
+    }
+})
 
 bot.login(process.env.TOKEN);
